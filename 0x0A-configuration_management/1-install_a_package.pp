374b7db04c3e3607c2634e3a10e63bc71ad6262a
francis waihiga <francisgwaihiga@gmail.com>
2023-11-24 19:31:14 +0000

    Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#File: Installs flask package from pip3
-package { 'flask':
-    ensure           => '2.1.0',
-    provider         => 'pip3',
-    uninstall_options => '--ignore-installed',
+# Installs a package flask from pip3
+package {'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
