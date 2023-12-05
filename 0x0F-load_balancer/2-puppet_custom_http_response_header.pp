60f39ea8c5c303cae63186671a4129e2a1501eae
francis waihiga <francisgwaihiga@gmail.com>
2023-12-05 21:45:14 +0000

    Advanced task solution

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,20 @@
 # creates a custom HTTP header response
 exec {'update':
     command  => 'sudo apt-get -y update',
+    provider => shell,
 }
 package {'Nginx':
-    ensure => installed
+    ensure => installed,
 }
 
 exec {'add_header':
     command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\
 tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
     environment => ['HOST={hostname}'],
+    provider    => shell,
 }
 
--> exec {'restart Nginx':
+exec {'restart Nginx':
     command  => 'sudo service nginx restart',
+    provider => shell,
 }
