a009ee61691fa3eb77b21f3424f40c5621816817
Francis Waihiga <francisgwaihiga@gmail.com>
2024-08-15 07:12:16 +0300

    Task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,7 @@
 
 """
 A function that queries the reddit API and prints the title of the first
-hot ports listed for a subreddit
+hot ports listed for a given subreddit
 """
 
 import requests
@@ -17,11 +17,11 @@ def top_ten(subreddit):
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'FirstRedditApp/1.0 (francisgwaihiga@gmail.com)'}
-    queries = {'limit': 10}
+    params = {'limit': 10}
 
     try:
         response = requests.get(
-            url=url, headers=headers, params=queries, allow_redirects=False)
+            url=url, headers=headers, params=params, allow_redirects=False)
         if response.status_code != 200:
             print(None)
         else:
