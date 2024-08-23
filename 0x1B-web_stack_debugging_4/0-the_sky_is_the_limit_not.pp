a071dab8d1ef3e8b47f38622a0234ccd8a01c211
Francis Waihiga <francisgwaihiga@gmail.com>
2024-08-23 03:52:02 +0300

    Task 0 - output error corrected

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,11 @@
 # A Puppet manifest to fix a server request failure under high load by updating Nginx settings
 
-file_line { 'increase nginx worker connections':
-  path   => '/etc/default/nginx',
-  line   => 'worker_connections 4096;',
-  match  => '^worker_connections\s+\d+;',
-  notify => Service['nginx'],
+exec {'write':
+  provider => shell,
+  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx'
 }
 
-service { 'nginx':
-  ensure => 'running',
-  enable => true,
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart'
 }
